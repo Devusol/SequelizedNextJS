d1cf3ce4939873b0d69c3fa7b69fd4f574b7a7b2
Sean P <sean@devusol.com>
2023-04-20 22:56:32 -0400

    testing findOrCreate methods

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,9 +1,19 @@
 import Users from "../../database/Models/Users"
 
-export default async function handler(req, res) {
-    console.log("test route", req.body)
 
+
+
+export default async function handler(req, res) {
+    console.log("test route", req.query)
+    const { name } = req.query
+   
     try {
+
+        await Users.findOrCreate({
+            where: {
+                Name: name ? `${name.split("_")[0]} ${name.split("_")[1]}` : "No Name"
+            }
+        });
 
         const users = await Users.findAll({ raw: true })
         console.log("Users: ", users)
